b64b230cf5e3555deb4b395680447a1f012ed603
martin hristev <martin.hristev@vodafone.com>
2023-05-16 20:41:55 +0100

    dasdas

--- main.py
+++ main.py
@@ -3,22 +3,18 @@
 from flask import Flask
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
-import os
-
 
 # Initialize the Flask app
 app = Flask(__name__)
 CORS(app)
 
 # Configure the Flask app and database connection
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://user:password@localhost:5432/mydatabase'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
-
 with app.app_context():
     ##from db_models.users import User
-    from db_models.users import UserProfile
     db.create_all()
 
 # Import your routes here
